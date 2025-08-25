a15f1d358c405129f3c99816d203fb5431122e51
SuccessfullyFailed <renzogerritzen@gmail.com>
2025-08-25 17:01:26 +0200

    Turn into working blender extension

--- __init__.py
+++ __init__.py
@@ -1,22 +1,27 @@
 bl_info = {
 	"name": "Localize texture images",
-	"blender": (4, 4, 0),
+	"blender": (2, 8, 0),
 	"category": "Material",
 	"author": "Renzo Gerritzen",
-	"description": "Copy all used image textures into a "material_textures" folder in the project file root dir and relink all image textures to the localized files them.",
+	"description": "Copy all used image textures into a \"material_textures\" folder in the project file root dir and relink all image textures to the localized files them.",
+	"location": "View3D > Sidebar > Tool",
+	"category": "3D View"
 }
+
+
 
 import bpy
 import os
 import shutil
 
 
-def localize_image_textures():
+
+def start_file_localization(context, subdir_name):
 	blend_dir = os.path.dirname(bpy.data.filepath)
 	if not blend_dir:
 		raise Exception("Please save your .blend file first!")
 
-	textures_dir = os.path.join(blend_dir, "material_textures")
+	textures_dir = os.path.join(blend_dir, subdir_name)
 	os.makedirs(textures_dir, exist_ok=True)
 
 	copied_images = {}
@@ -44,45 +49,48 @@ def localize_image_textures():
 	return "All images copied and relinked."
 
 
-class TEXTURE_OT_CopyAndRelink(bpy.types.Operator):
-	bl_idname = "texture.localize_image_textures"
-	bl_label = "Localize image textures"
-	bl_description = "Copy all used image textures into a "material_textures" folder in the project file root dir and relink all image textures to the localized files them."
-	bl_options = { 'REGISTER', 'UNDO' }
+
+class OBJECT_OT_start_localize(bpy.types.Operator):
+	bl_idname = "object.start_localize"
+	bl_label = "Localize texture files"
+	bl_description = "Executes my custom function"
 
 	def execute(self, context):
-		try:
-			result = localize_image_textures()
-			self.report({ 'INFO' }, result)
-		except Exception as e:
-			self.report({ 'ERROR' }, str(e))
-			return { 'CANCELLED' }
+		subdir_name = context.scene.localization_subdir_name
+		start_file_localization(context, subdir_name)
+		self.report({'INFO'}, f"Copied all texture files to: ./{subdir_name}")
 		return {'FINISHED'}
 
 
-class TEXTURE_LOCALIZER_CopyPanel	(bpy.types.Panel):
-	bl_label = "Relink texture images"
-	bl_idname = "TEXTURE_PT_copy_panel"
-	bl_space_type = 'VIEW_3D'
-	bl_region_type = 'UI'
-	bl_category = 'Textures'
+
+class VIEW3D_PT_localization_panel(bpy.types.Panel):
+	bl_label = "Localization"
+	bl_idname = "VIEW3D_PT_localization_panel"
+	bl_space_type = "VIEW_3D"
+	bl_region_type = "UI"
+	bl_category = "Tool"
 
 	def draw(self, context):
 		layout = self.layout
-		layout.operator("texture.localize_image_textures", icon="FILE_FOLDER")
+		layout.prop(context.scene, "localization_subdir_name", text="Subdir name")
+		layout.operator("object.start_localize")
 
 
-classes = (TEXTURE_OT_LocalizeImageTexture, TEXTURE_LOCALIZER_CopyPanel	)
+
+classes = (OBJECT_OT_start_localize, VIEW3D_PT_localization_panel)
+
 
 
 def register():
+	bpy.types.Scene.localization_subdir_name = bpy.props.StringProperty(name="Subdir name", description="The sub-directory to which the image-texture files will be copied", default="texture_images")
 	for cls in classes:
 		bpy.utils.register_class(cls)
-
 
 def unregister():
 	for cls in reversed(classes):
 		bpy.utils.unregister_class(cls)
+	del bpy.types.Scene.localization_subdir_name
+
 
 
 if __name__ == "__main__":
